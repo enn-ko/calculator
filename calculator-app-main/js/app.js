32a2f7b01bdc42846de2fd7ab6c1613897cc0a4e
enn-ko <ennko1898@gmail.com>
2022-12-15 21:09:05 +0630

    first

--- calculator-app-main/js/app.js
+++
@@ -1,5 +0,0 @@
-let calcArray1 = ['7','8','9','DEL','4','5','6','+','1','2','3','-','.','0','/','x']
-let calcArray2 = ['RESET','=']
-
-let numArray = document.querySelector('.numArray1')
-let div = document.createElement('div')
